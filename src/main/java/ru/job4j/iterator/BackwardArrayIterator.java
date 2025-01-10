9effae4346581019952f7dcc1841bcc29d22871a
RuslanRad <r140390@yandex.ru>
2025-01-10 20:25:21 +0500

    1. Что такое итератор. [13]

---
+++ src/main/java/ru/job4j/iterator/BackwardArrayIterator.java
@@ -0,0 +1,30 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BackwardArrayIterator implements Iterator<Integer> {
+    private final int[] data;
+    private int point;
+
+    public BackwardArrayIterator(int[] data) {
+        this.data = data;
+        this.point = data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (point < data.length && point >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
