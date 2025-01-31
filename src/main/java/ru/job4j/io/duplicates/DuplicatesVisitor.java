13c5429128d2c5803ab9163cee8d073225999f6f
RuslanRad <r140390@yandex.ru>
2025-01-31 22:03:10 +0500

    11. Поиск дубликатов - исправление [79]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     List<Path> duplicates = new ArrayList<>();
+
     @Override
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
