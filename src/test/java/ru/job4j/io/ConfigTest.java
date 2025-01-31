1c1801378366636bd74d0c02758e19e9fc77262d
RuslanRad <r140390@yandex.ru>
2025-01-31 13:41:19 +0500

    8. Тестирование IO [75]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -1,6 +1,12 @@
 package ru.job4j.io;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -74,5 +80,103 @@ class ConfigTest {
         Config config = new Config(path);
         config.load();
         assertThat(config.value("key1")).isEqualTo("value=1");
+    }
+
+    @Test
+    void whenNotKeyWithTempDir(@TempDir Path tempDir) throws IOException{
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)){
+            output.println("=value");
+        }
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Config config = new Config(source.getAbsolutePath());
+                    config.load();
+                });
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNotValueWithTempDir(@TempDir Path tempDir) throws IOException{
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)){
+            output.println("value=");
+        }
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = source.getAbsolutePath();
+                    Config config = new Config(path);
+                    config.load();
+                });
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNotKeyAndNotValueWithTempDir(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)){
+            output.println("=");
+        }
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = source.getAbsolutePath();
+                    Config config = new Config(path);
+                    config.load();
+                });
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenPairWithCommentWitchTempDir(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("#Comment");
+            output.println("java=the_best");
+        }
+        Config config = new Config(source.getAbsolutePath());
+        config.load();
+        assertThat(config.value("java")).isEqualTo("the_best");
+    }
+
+    @Test
+    void whenPairWithCommentAndEmptyLinesWithTempDir(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("#Comment");
+            output.println();
+            output.println();
+            output.println("java=the_best");
+            output.println();
+            output.println("php=bad");
+        }
+        Config config = new Config(source.getAbsolutePath());
+        config.load();
+        assertThat(config.value("java")).isEqualTo("the_best");
+        assertThat(config.value("php")).isEqualTo("bad");
+    }
+
+    @Test
+    void whenPairValueWithEquallyWithTempDir(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("java=the=");
+        }
+        Config config = new Config(source.getAbsolutePath());
+        config.load();
+        assertThat(config.value("java")).isEqualTo("the=");
+    }
+
+    @Test
+    void whenPairValueWithEquallyOneWithTempDir(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("java=the=1");
+        }
+        Config config = new Config(source.getAbsolutePath());
+        config.load();
+        assertThat(config.value("java")).isEqualTo("the=1");
     }
 }
