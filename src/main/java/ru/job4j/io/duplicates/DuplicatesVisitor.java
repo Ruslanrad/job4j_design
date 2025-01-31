07860e7e768ec49a3e8f311ad0218d2686fca8f0
RuslanRad <r140390@yandex.ru>
2025-01-31 22:00:13 +0500

    11. Поиск дубликатов [79]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,23 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    List<Path> duplicates = new ArrayList<>();
+    @Override
+    public FileVisitResult visitFile(Path file,
+                                     BasicFileAttributes attributes) throws IOException {
+        duplicates.add(file);
+        return super.visitFile(file, attributes);
+    }
+
+    public List<Path> getDuplicates() {
+        return duplicates;
+    }
+}
