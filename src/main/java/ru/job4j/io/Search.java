6d8182b26649e32b8178024ee801c5c483c74c92
RuslanRad <r140390@yandex.ru>
2025-02-01 17:19:17 +0500

    12. Валидация параметров запуска [82]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,20 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        argsValidate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void argsValidate(String[] arguments) {
+        if (arguments.length != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments.");
+        }
     }
 }
