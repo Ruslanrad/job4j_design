13451597696de11470c5f8d98da9cb4af64e7b48
RuslanRad <r140390@yandex.ru>
2025-02-24 20:17:45 +0500

    0. Что такое база данных. [111]

--- bd/users.sql
+++ bd/users.sql
@@ -1,1 +1,14 @@
+create table users(
+	id serial primary key,
+	name varchar(255),
+	birthday Date,
+	has_children BOOLEAN DEFAULT(FALSE) NOT NULL
+);
+
+insert into users (name, birthday, has_children) values ('Ruslan', '2000-10-10', TRUE);
+
 select * from users;
+
+update users set name = 'Artur';
+
+delete from users;
